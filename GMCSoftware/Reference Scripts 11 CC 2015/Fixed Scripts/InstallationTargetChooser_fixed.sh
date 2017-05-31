e205999025135a46ee25fe8c17c24e307671f8b6
foigus <ferguspa@yahoo.com>
2017-05-31 11:49:02 -0500

    Fixing another Call to InDesignChooser.app

---
+++ GMCSoftware/Reference Scripts 11 CC 2015/Fixed Scripts/InstallationTargetChooser_fixed.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname "$0"`
+
+#open "$SCRIPT_DIR/InDesignChooser.app"
+echo -n "/Applications/Adobe InDesign CC 2015/Plug-Ins" > /private/tmp/gmc-id-plugins
+
+exit 0
---
+++ GMCSoftware/Reference Scripts 11 CC 2015/Original GMC Software/InstallationTargetChooser.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_DIR=`dirname "$0"`
+
+open "$SCRIPT_DIR/InDesignChooser.app"
+
+exit 0
