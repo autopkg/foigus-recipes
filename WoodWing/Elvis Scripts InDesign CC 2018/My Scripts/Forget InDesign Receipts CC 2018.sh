bcb38598a9e81254b93f0330b6b8c100a2fcde4b
foigus <ferguspa@yahoo.com>
2019-03-06 08:11:16 -0600

    Adding Elvis InDesign CC 2018 Plugin Recipes

---
+++ WoodWing/Elvis Scripts InDesign CC 2018/My Scripts/Forget InDesign Receipts CC 2018.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+receiptOne=com.woodwing.pkg.lvsidcc2018
+receiptTwo=com.woodwing.pkg.payload
+
+if pkgutil --pkgs="${receiptOne}" 2>&1 > /dev/null
+then
+	pkgutil --forget "${receiptOne}"
+fi
+
+if pkgutil --pkgs="${receiptTwo}" 2>&1 > /dev/null
+then
+	pkgutil --forget "${receiptTwo}"
+fi
